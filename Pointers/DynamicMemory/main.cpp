5f7424cda3b893ab84e5e83de0023ee353bdecda
Clayman <Clayman@DESKTOP-DM8E77U>
2021-12-04 19:32:55 +0200

    DynamicMemory.

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -1,14 +1,26 @@
 ﻿#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
-void FillRand(int arr[], const int n);
+int** allocate(const int rows, const int cols);
+void clear(int** arr, const int rows);
+
+void FillRand(int* arr, const int n, int minRand = 0, int maxRand = 100);
+void FillRand(int** arr, const int rows, const int cols);
 void Print(int* arr, const int n);
+void Print(int** arr, const int rows, const int cols);
 
 int* push_back(int arr[], int& n, int value);
 int* push_front(int arr[], int& n, int value);
 int* pop_back(int arr[], int& n);
 
-#define DYNAMIC_MEMORY_1
+int** push_row_back(int** arr, int& rows, const int cols);
+void push_col_back(int** arr, const int rows, int& cols);
+
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
 
 void main()
 {
@@ -44,16 +56,65 @@ void main()
 	//Print(arr, n);	//Runtime error  
 #endif // DYNAMIC_MEMORY_1
 
+	int rows;	//Количество строк
+	int cols;	//Количество элементов строки (столбцов)
+	std::cout << "Введите количество строк: "; cin >> rows;
+	cout << "Введите количество элементов строки: "; cin >> cols;
 
+	int** arr = allocate(rows, cols);
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+	cout << "\n-----------------------------------------\n";
+	arr = push_row_back(arr, rows, cols);
+	FillRand(arr[rows - 1], cols, 100, 1000);
+	Print(arr, rows, cols);
+	cout << "\n-----------------------------------------\n";
+	push_col_back(arr, rows, cols);
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i][cols - 1] = rand();
+	}
+	Print(arr, rows, cols);
+	
+	clear(arr, rows);
 }
 
-void FillRand(int arr[], const int n)
+int** allocate(const int rows, const int cols)
+{
+	int** arr = new int* [rows] {};
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int [cols] {};
+	}
+	return arr;
+}
+void clear(int** arr, const int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	delete[] arr;
+}
+
+void FillRand(int* arr, const int n, int minRand, int maxRand)
 {
 	//Обращение к элементам динамического массива:
 	for (int i = 0; i < n; i++)
 	{
 		//Через арифметику указателей и оператор разыменования:
-		*(arr + i) = rand() % 100;
+		*(arr + i) = rand() % (maxRand-minRand)+minRand;
+	}
+}
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
 	}
 }
 void Print(int* arr, const int n)
@@ -65,6 +126,17 @@ void Print(int* arr, const int n)
 	//	//Оператор индексирования возвращает значение по индексу.
 	//}
 	cout << endl;
+}
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
 }
 
 int* push_back(int arr[], int& n, int value)
@@ -110,4 +182,25 @@ int* pop_back(int arr[], int& n)
 	for (int i = 0; i < n; i++)buffer[i] = arr[i];
 	delete[] arr;
 	return buffer;
+}
+
+int** push_row_back(int** arr, int& rows, const int cols)
+{
+	int** buffer = new int*[rows + 1]{};
+	for (int i = 0; i < rows; i++)buffer[i] = arr[i];
+	delete[] arr;
+	buffer[rows] = new int[cols] {};
+	rows++;
+	return buffer;
+}
+void push_col_back(int** arr, const int rows, int& cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		int* buffer = new int[cols + 1]{};
+		for (int j = 0; j < cols; j++)buffer[j] = arr[i][j];
+		delete[] arr[i];
+		arr[i] = buffer;
+	}
+	cols++;
 }
